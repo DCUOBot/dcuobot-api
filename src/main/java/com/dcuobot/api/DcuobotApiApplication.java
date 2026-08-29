7e6a87f28d62bb39cbb21688b94d304e39c43bd7
Hype <michael.tillmanns@icloud.com>
2026-08-29 13:45:50 +0200

    initialize Spring Boot project structure

---
+++ src/main/java/com/dcuobot/api/DcuobotApiApplication.java
@@ -0,0 +1,11 @@
+package com.dcuobot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcuobotApiApplication {
+    static void main(String[] args) {
+        SpringApplication.run(DcuobotApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dcuobot/api/DcuobotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcuobot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcuobotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
